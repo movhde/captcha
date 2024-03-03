25b68a0de5d470c0eae133a8888dc26f03425f94
movahede salehivaziri <movahede@movahedes-MacBook-Air.local>
2024-03-03 18:28:24 +0330

    insert data to database

--- src/postedData.php
+++ src/postedData.php
@@ -1,5 +1,6 @@
 <?php
 include "utilities.php";
+$pdo = require_once "Connection.php";
 
 $inputSum = convertPersianNumbersToEnglish($_POST['sum']);
 $sum = $_SESSION['number1'] + $_SESSION['number2'];
@@ -9,9 +10,20 @@ if ($inputSum === $sum) {
   echo "karet dorosteeeee";
   unset($_SESSION['number1']);
   unset($_SESSION['number2']);
+  header("Location: http://localhost/captchaProject/src/welcome.php");
 } else {
   $_SESSION['incorrect_sum'] = true;
   unset($_SESSION['number1']);
   unset($_SESSION['number2']);
   header("Location: http://localhost/captchaProject/src/");
 }
+
+$sql = "insert into users(name, lastName, age) VALUES(:name, :lastName, :age)";
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->execute([
+  'name' => $_POST['u_name'],
+  'lastName' => $_POST['u_lastName'],
+  'age' => convertPersianNumbersToEnglish($_POST['u_age']),
+]);
