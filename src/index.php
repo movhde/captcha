9f0429619634a71d6b947a9d8084bd73d36eb9a2
movahede salehivaziri <movahede@movahedes-MacBook-Air.local>
2024-03-05 17:05:49 +0330

    update inputs validation

--- src/index.php
+++ src/index.php
@@ -20,16 +20,47 @@ include "CaptchaImage.php";
         <h3 class="text-white text-xl text-center">اطلاعات خود را وارد کنید</h3>
         <div class="flex flex-col gap-3">
           <label class="text-base text-sm text-white">نام:</label>
-          <input type="text" name="u_name" placeholder="نام خود را وارد کنید" class="text-sm bg-white focus:shadow-lg focus:shadow-black-500/40 border-2 border-transparent focus:outline-none p-1.5 rounded-xl">
+          <input type="text" name="u_name" placeholder="نام خود را وارد کنید" class="text-sm focus:shadow-lg focus:shadow-black-500/40 border-2 focus:outline-none p-1.5 rounded-xl
+          <?php if (isset($_SESSION['hasNameErr']) && $_SESSION['hasNameErr'] == 1) : ?> bg-red-200 border-red-200 <?php else : ?> bg-white border-transparent <?php endif; ?>">
         </div>
+        <?php if (isset($_SESSION['hasNameErr']) && $_SESSION['hasNameErr'] == 1) : ?> <span class="text-red-400 -mt-2">ورود نام الزامیست.</span> <?php endif; ?>
         <div class="flex flex-col gap-3">
           <label class="text-base text-sm text-white">نام خانوادگی:</label>
-          <input type="text" name="u_lastName" placeholder="نام خانوادگی خود را وارد کنید" class="text-sm bg-white focus:shadow-lg focus:shadow-black-500/40 border-2 border-transparent focus:outline-none p-1.5 rounded-xl">
+          <input type="text" name="u_lastName" placeholder="نام خانوادگی خود را وارد کنید" class="text-sm focus:shadow-lg focus:shadow-black-500/40 border-2 focus:outline-none p-1.5 rounded-xl
+          <?php if (isset($_SESSION['hasLastNameErr']) && $_SESSION['hasLastNameErr'] == 1) : ?> bg-red-200 border-red-200 <?php else : ?> bg-white border-transparent <?php endif; ?>">
         </div>
+        <?php if (isset($_SESSION['hasLastNameErr']) && $_SESSION['hasLastNameErr'] == 1) : ?> <span class="text-red-400 -mt-2">ورود نام خانوادگی الزامیست.</span> <?php endif; ?>
         <div class="flex flex-col gap-3">
           <label class="text-base text-white">سن:</label>
-          <input type="text" name="u_age" onkeypress="return /[۰-۹ | 0-9\s]/i.test(event.key)" placeholder="سن خود را وارد کنید" class="text-sm bg-white focus:shadow-lg focus:shadow-[#f0fbea]-500/40 border-2 border-transparent focus:outline-none p-1.5 rounded-xl">
+          <input type="text" name="u_age" onkeypress="return /[۰-۹ | 0-9\s]/i.test(event.key)" placeholder="سن خود را وارد کنید" class="text-sm focus:shadow-lg focus:shadow-[#f0fbea]-500/40 border-2 focus:outline-none p-1.5 rounded-xl
+          <?php if (isset($_SESSION['hasAgeErr']) && $_SESSION['hasAgeErr'] == 1) : ?> bg-red-200 border-red-200 <?php else : ?> bg-white border-transparent <?php endif; ?>">
         </div>
+        <?php if (isset($_SESSION['hasAgeErr']) && $_SESSION['hasAgeErr'] == 1) : ?> <span class="text-red-400 -mt-2">ورود سن الزامیست.</span> <?php endif; ?>
+        <div class="flex flex-col gap-3">
+          <label class="text-base text-white">ایمیل:</label>
+          <input type="email" name="u_email" placeholder="ایمیل خود را وارد کنید" class="text-sm focus:shadow-lg focus:shadow-[#f0fbea]-500/40 border-2 focus:outline-none p-1.5 rounded-xl
+          <?php if (isset($_SESSION['hasEmailErr']) && $_SESSION['hasEmailErr'] == 1) : ?> bg-red-200 border-red-200 <?php else : ?> bg-white border-transparent <?php endif; ?>">
+        </div>
+        <?php if (isset($_SESSION['hasEmailErr']) && $_SESSION['hasEmailErr'] == 1) : ?> <span class="text-red-400 -mt-2">ورود ایمیل الزامیست.</span> <?php endif; ?>
+        <div class="flex flex-col gap-3">
+          <label class="text-base text-white">رمز عبور:</label>
+          <input type="password" name="u_pass" placeholder="رمز عبور خود را وارد کنید" class="text-sm focus:shadow-lg focus:shadow-[#f0fbea]-500/40 border-2 focus:outline-none p-1.5 rounded-xl
+          <?php if (isset($_SESSION['hasPassErr']) && $_SESSION['hasPassErr'] == 1) : ?>
+            bg-red-200 border-red-200 
+          <?php else : ?> 
+            <?php if (isset($_SESSION['invalidPass']) && $_SESSION['invalidPass'] == 1) : ?>
+              bg-red-200 border-red-200 
+            <?php else : ?>  
+              bg-white border-transparent 
+            <?php endif; ?>
+          <?php endif; ?>">
+        </div>
+        <?php if (isset($_SESSION['hasPassErr']) && $_SESSION['hasPassErr'] == 1) : ?>
+          <span class="text-red-400 -mt-2">ورود رمز عبور الزامیست.</span>
+        <?php endif; ?>
+        <?php if (isset($_SESSION['invalidPass']) && $_SESSION['invalidPass'] == 1 &&  $_SESSION['hasPassErr'] == 0) : ?>
+          <span class="text-red-400 -mt-2">رمز عبور انتخابی باید حداقل دارای یک حرف بزرگ یک حرف کوچک و یک عدد باشد.</span>
+        <?php endif; ?>
         <div class="flex flex-col gap-3">
           <label class="text-base text-white">حاصل عبارت را در کادر زیر وارد کنید:</label>
           <div class="flex gap-2">
--- src/postedData.php
+++ src/postedData.php
@@ -4,26 +4,88 @@ $pdo = require_once "Connection.php";
 
 $inputSum = convertPersianNumbersToEnglish($_POST['sum']);
 $sum = $_SESSION['number1'] + $_SESSION['number2'];
+$firstName = $_POST['u_name'];
+$lastName = $_POST['u_lastName'];
+$age = $_POST['u_age'];
+$email = $_POST['u_email'];
+$pass = $_POST['u_pass'];
+$passPattern = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/';
+
+if (empty($firstName)) {
+  $_SESSION['hasNameErr'] = true;
+  $cnt1 = 0;
+} else {
+  $_SESSION['hasNameErr'] = false;
+  $cnt1 = 1;
+}
+
+if (empty($lastName)) {
+  $_SESSION['hasLastNameErr'] = true;
+  $cnt2 = 0;
+} else {
+  $_SESSION['hasLastNameErr'] = false;
+  $cnt2 = 1;
+}
+
+if (empty($age)) {
+  $_SESSION['hasAgeErr'] = true;
+  $cnt3 = 0;
+} else {
+  $_SESSION['hasAgeErr'] = false;
+  $cnt3 = 1;
+}
+
+if (!empty($email) || filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  $_SESSION['hasEmailErr'] = false;
+  $cnt5 = 1;
+} else {
+  $_SESSION['hasEmailErr'] = true;
+  $cnt5 = 0;
+}
+
+if (!preg_match($passPattern, $pass)) {
+  $_SESSION['invalidPass'] = true;
+  $cnt6 = 0;
+} else {
+  $_SESSION['invalidPass'] = false;
+  $cnt6 = 1;
+}
+
+if (empty($pass)) {
+  $_SESSION['hasPassErr'] = true;
+  $cnt7 = 0;
+} else {
+  $_SESSION['hasPassErr'] = false;
+  $cnt7 = 1;
+}
 
 if ($inputSum === $sum) {
   $_SESSION['incorrect_sum'] = false;
-  echo "karet dorosteeeee";
+  $cnt4 = 1;
   unset($_SESSION['number1']);
   unset($_SESSION['number2']);
-  header("Location: http://localhost/captchaProject/src/welcome.php");
 } else {
   $_SESSION['incorrect_sum'] = true;
+  $cnt4 = 0;
   unset($_SESSION['number1']);
   unset($_SESSION['number2']);
-  header("Location: http://localhost/captchaProject/src/");
 }
 
-$sql = "insert into users(name, lastName, age) VALUES(:name, :lastName, :age)";
+if ($cnt1 + $cnt2 + $cnt3 + $cnt4 + $cnt5 + $cnt6 + $cnt7 === 7) {
+  $sql = "insert into users(name, lastName, age, email, password) VALUES(:name, :lastName, :age, :email, :password)";
 
-$stmt = $pdo->prepare($sql);
+  $stmt = $pdo->prepare($sql);
 
-$stmt->execute([
-  'name' => $_POST['u_name'],
-  'lastName' => $_POST['u_lastName'],
-  'age' => convertPersianNumbersToEnglish($_POST['u_age']),
-]);
+  $stmt->execute([
+    'name' => $firstName,
+    'lastName' => $lastName,
+    'email' => $email,
+    'password' => password_hash($pass, PASSWORD_BCRYPT, ['cost' => 12]),
+    'age' => convertPersianNumbersToEnglish($age)
+  ]);
+
+  session_destroy();
+  header("Location: http://localhost/captchaProject/src/welcome.php");
+} else {
+  header("Location: http://localhost/captchaProject/src");
+}
