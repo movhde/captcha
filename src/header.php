26f874b279bb2edfa1200c9329a6fa57d4fcab86
movahede salehivaziri <movahede@movahedes-MacBook-Air.local>
2024-03-05 20:26:07 +0330

    add login page

--- src/header.php
+++ src/header.php
@@ -1,6 +1,9 @@
 <nav class="bg-[#4F6F52] w-full h-24 flex">
-  <div class="w-1/4 flex  items-center justify-center">
-    <a href="http://localhost/captchaProject/src/" class="w-28 text-black text-center bg-[#f0fbea] p-4 cursor-pointer rounded-lg hover:bg-transparent hover:border hover:border-[#f0fbea] hover:text-white transition duration-300">
+  <div class="w-1/4 flex gap-5 items-center justify-start mr-5">
+    <a href="http://localhost/captchaProject/src/logout.php" class="w-28 text-black text-center bg-[#f0fbea] p-4 cursor-pointer rounded-lg hover:bg-transparent hover:border hover:border-[#f0fbea] hover:text-white transition duration-300">
+      خروج
+    </a>
+    <a href="http://localhost/captchaProject/src/" class="w-28 text-white text-center border border-[#f0fbea] p-4 cursor-pointer rounded-lg hover:bg-[#f0fbea] hover:opacity-85 hover:text-black transition duration-300">
       خانه
     </a>
   </div>
